83dfd05870b4db98b9af6b15de780b61bc35b092
enock-jufri <enockjufri@gmail.com>
2025-03-05 14:12:58 +0300

    mpesa callback

--- lib/daraja.py
+++ lib/daraja.py
@@ -8,7 +8,7 @@ from flask import Flask, request, jsonify
 
 BUSINESS_SHORTCODE = "174379"  # Use your PayBill/Till number
 PASSKEY = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919" 
-CALLBACK_URL = "https://4a1d-41-90-68-99.ngrok-free.app/mpesa/callback"  # Use your server URL
+CALLBACK_URL = "https://bank-db.onrender.com/mpesa/callback"  # Use your server URL
 CONSUMER_KEY = "9pq9aT7AkzSSUEkXT0A5TU1p6xaqkJzjcUm4pSVUmzGsImlo"
 CONSUMER_SECRET = "DVb8u8KoAGTwW88EVcSAw8HAJnGGBAx1OHkWdJh4bjk7qo4Ub2bPjQY9c9LekCSi"
 SECURITY_CREDENTIAL="b5SrQ2MCWXsi1/Ek6A7qKm1mfhFCHEQkScTgYMGrVZbrIvfHLijDzcCFVy7sgDorIbScXYZy/y7OrK/sIzwkZ3RIJ9KPamj1g+DhHY1NGlnDCWLtpXP68eeXjIAJazwmoqHBiGjV4mkCNiW5Qo8a4jwPLQe3256lSVoeYNcStsGJ956ZRFA2UUk6VccpExdkJUmMenHIRcEk7zAHc1qS19Lyu7vHRwVs9G/6UwT56S1BKf/xmHHCWhrwNIOtE+Adie7l9f9bNJzl3vsvTdsGbaCCwojePbRgzBHUA2eW0rbQV5OJvdeDdbfCE5BDzt5L2opJ3JqYqyA3Jcf4oMsbJw=="
